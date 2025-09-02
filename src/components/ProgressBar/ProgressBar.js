34bfd6d6f998bf88121129b91674db888c7bcd65
ractaylor-amplify <ractaylor@amplify.com>
2025-09-02 15:03:15 -0400

    complete progressbar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,61 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+const SIZES = {
+  large: {
+    "--borderRadius": "8px",
+    "--height": "24px",
+    "--barheight": "16px",
+    "--padding": "4px"
+  },
+  medium: {
+    "--borderRadius": "4px",
+    "--height": "12px",
+    "--barheight": "12px",
+    "--padding": "0"
+  },
+  small: {
+    "--borderRadius": "4px",
+    "--height": "8px",
+    "--barheight": "8px",
+    "--padding": "0"
+  }
 };
+
+const ProgressBar = ({ value, size }) => {
+  const styles = SIZES[size];
+
+  return <StyledProgressBar role="progressbar" aria-valuenow={value} aria-valuemin="0" aria-valuemax="100" style={styles}>
+    <BarContainer size={size}>
+      <Bar value={value}>
+        <VisuallyHidden>{value}%</VisuallyHidden>
+      </Bar>
+    </BarContainer>
+  </StyledProgressBar>;
+};
+
+const StyledProgressBar = styled.div`
+  position: relative;
+  width: 370px;
+  border-radius: var(--borderRadius);
+  height: var(--height);
+  padding: var(--padding);
+  background: ${COLORS.transparentGray15};
+  box-shadow: 0px 2px 4px 0px ${COLORS.transparentGray35} inset;
+`;
+
+const BarContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  border-radius: ${props => props.size === 'large' ? 'calc(var(--borderRadius) / 2)' : 'var(--borderRadius)'};
+  overflow: hidden;
+`;
+
+const Bar = styled.div`
+  width: ${props => props.value}%;
+  height: 100%;
+  background: ${COLORS.primary};
+`;
 
 export default ProgressBar;
